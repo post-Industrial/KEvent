1b3d3a72660106e448c5c0c91b4d2ef499da950e
Yuriy Nishenko <Djonson40@gmail.com>
2016-11-26 19:53:56 +0200

    add bad datepicker, and some fix

--- js/file.js
+++ js/file.js
@@ -1,8 +1,20 @@
+
 var map;
 function initMap() {
-  alert("ss");
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 50.448927, lng: 30.459067},
+    zoom: 15
   });
 }
+$(function () {
+        $('#datetimepicker6').datetimepicker();
+        $('#datetimepicker7').datetimepicker({
+            useCurrent: false //Important! See issue #1075
+        });
+        $("#datetimepicker6").on("dp.change", function (e) {
+            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+        });
+        $("#datetimepicker7").on("dp.change", function (e) {
+            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+        });
+    });
