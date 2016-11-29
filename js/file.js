cda4e2119ada1d6699b782601707529318960a42
Yuriy Nishenko <Djonson40@gmail.com>
2016-11-29 17:12:56 +0200

    changed checkbox and dp

--- js/file.js
+++ js/file.js
@@ -1,20 +1,23 @@
 
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 50.448927, lng: 30.459067},
-    zoom: 15
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 50.448927, lng: 30.459067 },
+        zoom: 15
+    });
 }
 $(function () {
-        $('#datetimepicker6').datetimepicker();
-        $('#datetimepicker7').datetimepicker({
-            useCurrent: false //Important! See issue #1075
-        });
-        $("#datetimepicker6").on("dp.change", function (e) {
-            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-        });
-        $("#datetimepicker7").on("dp.change", function (e) {
-            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-        });
+    $('#datetimepicker6').datetimepicker({
+        sideBySide: true
     });
+    $('#datetimepicker7').datetimepicker({
+        useCurrent: false, //Important! See issue #1075
+        sideBySide: true
+    });
+    $("#datetimepicker6").on("dp.change", function (e) {
+        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+    });
+    $("#datetimepicker7").on("dp.change", function (e) {
+        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+    });
+});
